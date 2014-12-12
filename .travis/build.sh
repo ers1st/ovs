c68d91e45c62b1af65a6b01a2b7594f56a811ac8
Davide De Rubeis <davidederob@gmail.com>
2014-12-12 11:26:08 +0100

    Cambiata versione del kernel delle macchine di Travis CI

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,8 +9,8 @@ CFLAGS=""
 function install_kernel()
 {
     wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.14.26.tar.gz
-    tar xzvf linux-3.16.2.tar.gz > /dev/null
-    cd linux-3.16.2
+    tar xzvf linux-3.14.26.tar.gz > /dev/null
+    cd linux-3.14.26
     make allmodconfig
     make net/openvswitch/
     KERNELSRC=$(pwd)
