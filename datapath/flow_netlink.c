a67825d3a9fca2f780bbbc7bbe0aa49356dbe9d5
Federico Di Domenicantonio <fededido91@hotmail.it>
2014-12-18 18:09:57 +0100

    Risolto merge conflict in flow_netlink.c.

--- datapath/flow_netlink.c
+++ datapath/flow_netlink.c
@@ -728,10 +728,10 @@ static int ovs_key_from_nlattrs(struct sw_flow_match *match, u64 attrs,
 		attrs &= ~(1ULL << OVS_KEY_ATTR_ND);
 	}
 
-	if (attrs & (1ULL << OVS_KEY_ATTR_STATE)) {
-			const struct ovs_key_state *state_key;
-			/*TODO*/
-	}
+	// if (attrs & (1ULL << OVS_KEY_ATTR_STATE)) {
+	// 		const struct ovs_key_state *state_key;
+	// 		/*TODO*/
+//	}
 
 	if (attrs != 0)
 		return -EINVAL;
